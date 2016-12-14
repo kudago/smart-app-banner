6d7dd8e3b5896750a8bbea58791d2ce1d2f6421c
Ryan Sundberg <ryan.sundberg@gmail.com>
2016-12-13 22:16:11 -0800

    Do not show the banner if the device's store is not configured

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ var SmartBanner = function (options) {
 	// - user is on mobile safari for ios 6 or greater (iOS >= 6 has native support for SmartAppBanner)
 	// - running on standalone mode
 	// - user dismissed banner
-	var unsupported = !this.type;
+	var unsupported = !this.type || !this.options.store[this.type];
 	var isMobileSafari = (this.type === 'ios' && agent.browser.name === 'Mobile Safari' && Number(agent.os.version) >= 6);
 	var runningStandAlone = navigator.standalone;
 	var userDismissed = cookie.get('smartbanner-closed');
